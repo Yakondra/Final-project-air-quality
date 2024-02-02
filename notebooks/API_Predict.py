4a783ea4e096d121d37410c7b759416b7821e1bd
Yakondra <140064177+Yakondra@users.noreply.github.com>
2024-02-02 22:27:36 +0000

    wip

--- notebooks/API_Predict.py
+++ notebooks/API_Predict.py
@@ -2,11 +2,8 @@ import streamlit as st
 from pickle import load
 import pickle
 
-dicc_area = load(open('../data/interim/diccionarios/dicc_area.pk', 'rb'))
-dicc_ccaa = load(open('../data/interim/diccionarios/dicc_ccaa.pk', 'rb'))
-dicc_estacion = load(open('../data/interim/diccionarios/dicc_estacion.pk', 'rb'))
-dicc_magnitudes = load(open('../data/interim/diccionarios/dicc_magnitudes.pk', 'rb'))
 dicc_municipios = load(open('../data/interim/diccionarios/dicc_municipios.pk', 'rb'))
+dicc_magnitudes = load(open('../data/interim/diccionarios/dicc_magnitudes.pk', 'rb'))
 dicc_provincias = load(open('../data/interim/diccionarios/dicc_provincias.pk', 'rb'))
 
 
