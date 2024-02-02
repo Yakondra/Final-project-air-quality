f8e514f9809aa2b76a5c2de901cca5c635572b40
Yakondra <140064177+Yakondra@users.noreply.github.com>
2024-02-02 19:10:11 +0000

    STREAMLIT WIP

--- notebooks/API_Predict.py
+++ notebooks/API_Predict.py
@@ -8,13 +8,13 @@ model_path = os.path.join('models', 'RandomForestMadrid_23.pk')
 model = joblib.load(model_path)
 
 
-input_data = {
-    'LATITUD': valor_latitud,
-    'LONGITUD': valor_longitud,
-    'MEDIA_DIARIA': valor_media_diaria,
-    'N_MUNICIPIO_N': valor_municipio,
-    'MAGNITUD_N': valor_magnitud,
-    'TIPO_AREA_N': valor_tipo_area,
-    'TIPO_ESTACION_N': valor_tipo_estacion,
-    'PELIGROSIDAD_N': valor_peligrosidad
-}
+# input_data = {
+#     'LATITUD': valor_latitud,
+#     'LONGITUD': valor_longitud,
+#     'MEDIA_DIARIA': valor_media_diaria,
+#     'N_MUNICIPIO_N': valor_municipio,
+#     'MAGNITUD_N': valor_magnitud,
+#     'TIPO_AREA_N': valor_tipo_area,
+#     'TIPO_ESTACION_N': valor_tipo_estacion,
+#     'PELIGROSIDAD_N': valor_peligrosidad
+# }
