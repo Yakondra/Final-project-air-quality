c3e7d905e7f6e0c87721e26d230407c6789d220e
Yakondra <140064177+Yakondra@users.noreply.github.com>
2024-02-04 22:00:44 +0000

    TERMINADO POR FIN SI DIOS QUIERE

--- notebooks/API_Predict.py
+++ notebooks/API_Predict.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 from pickle import load
 
-
 dicc_municipality = load(open('../data/interim/diccionarios/N_MUNICIPIO_dicc.pk', 'rb'))
 dicc_magnitudes = load(open('../data/interim/diccionarios/MAGNITUD_dicc.pk', 'rb'))
 dicc_area = load(open('../data/interim/diccionarios/TIPO_AREA_dicc.pk', 'rb'))
@@ -15,12 +14,6 @@ st.write('Introduzca los siguientes valores para iniciar.')
 
 #Users data
 
-mean_value = st.slider('Introduzca el valor recogido:',
-                    min_value = 0.04,
-                    max_value = 800.00,
-                    step = 0.01
-                    )
-
 sel_municip = st.selectbox('Seleccione el municipio:', options= list(dicc_municipality.keys()))
 
 sel_magnitud = st.selectbox('Seleccione el contaminante:', options= list(dicc_magnitudes.keys()))
@@ -29,27 +22,45 @@ sel_area = st.selectbox('Seleccione el tipo de área:', options= list(dicc_area.
 
 sel_station = st.selectbox('Seleccione el tipo de estación:', options= list(dicc_station.keys()))
 
+mean_value = st.number_input('Introduzca el valor recogido (µg/m3):',
+                    min_value = 0.04,
+                    max_value = 800.00,
+                    step = 0.01
+                    )
+
 
 # Load factorized values
 
-fact_values = load(open('../data/interim/factorize_values/sin_LL.pk', 'rb'))
+#fact_values = load(open('../data/interim/factorize_values/madrid_sin_LL.pk', 'rb'))
+
+row = [
+    dicc_municipality[sel_municip],
+    dicc_magnitudes[sel_magnitud],
+    dicc_area[sel_area],
+    dicc_station[sel_station],
+    mean_value
+]   
+
 
 # Button to predict
 
-row = [mean_value,
-    fact_values["N_MUNICIPIO_N"][sel_municip.lower()],
-    fact_values["MAGNITUD_N"][sel_magnitud.lower()],
-    fact_values["TIPO_AREA_N"][sel_area.lower()],
-    fact_values["TIPO_ESTACION_N"][sel_station.lower()]
-    ]
-
 if st.button('Predict:'):
+    #fact_row = fact_values.transform([row])[0]
 
     model = load(open('../models/RandomForestMadrid_23.pk', 'rb'))
+
     y_pred = model.predict([row])
+    y_result = round(y_pred[0], 2)
 
-    st.text('Peligrosidad:' +str(round(y_pred[0, 0], 2)))
 
+    if y_pred == 0:
+        result = "😎 BAJO 😎"
+    elif y_pred == 1:
+        result = "⛅ NORMAL ⛅"
+    elif y_pred == 2:
+        result = "⚠️ ALTO ⚠️"
+
+    st.text('Peligrosidad:' + result)
 
 
 
